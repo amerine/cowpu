abc3bff6be18f91acb55b95fd58337c309c5b36a
Timmy Crawford <timmydcrawford@gmail.com>
2010-05-05 09:33:35 -0700

    heroku configs

--- config/database.rb
+++ config/database.rb
@@ -12,6 +12,6 @@ postgres = URI.parse(ENV['DATABASE_URL'] || '')
 
 case Padrino.env
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "development.db"))
-  when :production  then DataMapper.setup(:default, "postgres://#{postgres.user}:#{postgres.password}@#{postgres.host}/#{postgres.user}"))
+  when :production  then DataMapper.setup(:default, "postgres://#{postgres.user}:#{postgres.password}@#{postgres.host}/#{postgres.user}")
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "test.db"))
 end
