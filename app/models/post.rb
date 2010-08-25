13e38aabf8a64b3cc47d95e1fb60f6c46393ffac
Timmy Crawford <timmydcrawford@gmail.com>
2010-08-24 21:19:10 -0700

    adding rss feed generator

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post
   property :created_at, DateTime
   property :account_id, Integer
   before :create, :generate_slug
+  after :save, :build_rss
   
   belongs_to :account
   has_tags
@@ -22,6 +23,32 @@ class Post
       count += 1
     end
     self.slug = new_slug
+  end
+  
+  def build_rss
+    feedr = Padrino.root('public','system','feed.xml')
+    out = ''
+    xml = Builder::XmlMarkup.new(:indent=>2,:target=>out)
+    xml.instruct! :xml, :version => "1.0"
+    xml.rss :version => "2.0" do
+      xml.channel do
+        xml.title "COWPU"
+        xml.description "Central Oregon Web Professionals Usergroup."
+        xml.link "http://cowpu.com"
+
+        self.all(:limit => 10,order => [ :created_at.desc ]).each do |post|
+          xml.item do
+            xml.title post.title
+            xml.link "http://cowpu.com/#{post.slug}"
+            xml.description { xml.cdata!(post.body) }
+            xml.pubDate post.created_at.to_s
+            xml.guid "http://cowpu.com/#{post.slug}"
+          end
+        end
+      end
+    end
+
+    File.open(feedr,'w'){|f| f.write(out) }
   end
 
   def self.recent_posts
