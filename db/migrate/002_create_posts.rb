090fba4b621cc208513a381b017e3a01939b4611
Mark Turner <mark@amerine.net>
2010-08-31 17:41:17 -0700

    fixed migration typo

--- db/migrate/002_create_posts.rb
+++ db/migrate/002_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title, :slug
       t.text :body
-      t.timetamps
+      t.timestamps
     end
   end
   
