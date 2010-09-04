fee7620d5f08f6d8c56a60c7d492fcc211f6f8dc
Mark Turner <mark@amerine.net>
2010-09-03 23:53:19 -0700

    made the meetings page render

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ CowpuAmerine::Application.routes.draw do
   resources :members do
     get 'signup', :on => :collection
   end
+  
+  get 'meetings', :to => "pages#meetings"
   root :to => "pages#index"
 end
